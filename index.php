7cdbe335ffb0429fb13e20880bf38199aac27118
LuigiTroiano <troiano.luigi8@gmail.com>
2021-02-01 13:41:08 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!-- Istruzioni:
+Riscrivere questa pagina del sito google
+https://policies.google.com/faq.
+Ci sono diverse domande con relative risposte.
+Gestire il “Database” e la visualizzazione di queste domande e risposte con PHP. -->
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Google FAQ</title>
+  </head>
+
+  <body>
+
+  </body>
+</html>
